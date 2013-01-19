6c26aa3981475f189925d3c922637e4ab81e3aa2
Luis Feliphe Silva Costa <luis.feliphe@dce.ufpb.br>
2013-01-19 12:42:24 -0300

    integrando classes

---
+++ GerentePassageiros.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from Passageiro import Passageiro
+import os
+
+class GerentePassageiros(object):
+
+	def lerArquivo(self, var):
+		if (os.path.isfile(var)):
+			arquivo = open(var)
+			lista =  arquivo.readlines()
+			arquivo.close()
+			return lista
+		return []
+
+	def funcaoOrdenacao(self,a,b):
+		if (a[1] == b[1]):
+			return (-1) * cmp(a[0], b[0])
+		return cmp(a[1], b[1])
+	
+	def organizarDados(self, dados):
+		cont = 0 		
+		dadosSplit =[]
+		#dando o split nos dados 
+		for i in dados:
+			if (cont == 0):
+				cont = cont + 1
+				pass
+			dadosSplit.append(i.split(","))
+		
+		listaPassageirosSplit = []
+		for i in dadosSplit:
+			if ((i[0] == "0" )or( i[0]=="1")):
+				listaPassageirosSplit.append(i)
+
+		#cria objetos
+		listaPassageiros = []
+		for i in listaPassageirosSplit:
+			listaPassageiros.append(Passageiro(i[0],i[1], i[2],i[3], i[4], i[5],i[6],i[7],i[8],i[9],i[10],i[11] ))
+		return listaPassageiros
+		
+	def __init__ (self, endereco):
+		self.endereco = endereco
+		dadosAMinerar = self.lerArquivo(endereco)
+		self.lista = self.organizarDados(dadosAMinerar)
+		self.mapaPalavras = {}
+		
+	def getLista (self):
+		return self.lista
+		
--- Persistencia.py
+++ Persistencia.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import math
 from Passageiro import Passageiro
-#import pickle
+from GerentePassageiros import GerentePassageiros
 import os
 
-M = 10
+M = 200
 
 def gravarArquivo(ranking):
 	arquivo = open("Ranking.dat", "wb")
@@ -12,86 +12,51 @@ def gravarArquivo(ranking):
 	arquivo.close()
 	
 def calculaDistancia (x1, x2, y1, y2):
-	if (x1 == "" or x2=="" or y1=="" or y2==""or x1 == " "):
-		return 1000
 	x1 = float (x1)
 	x2 = float (x2)
 	y1 = float (y1)
 	y2 = float (y2)
-	
+	print ("---------")
+	print x1 , y1, x2, y2
 	return math.sqrt( ((x2-x1)**2 ) + (y2-y1)**2)
-		
 	
-def lerArquivo(var):
-	if (os.path.isfile(var)):
-		arquivo = open(var)
-		lista =  arquivo.readlines()
-		arquivo.close()
-		return lista
-	return []
 
-def funcaoOrdenacao(self,a,b):
-	if (a[1] == b[1]):
-		return (-1) * cmp(a[0], b[0])
-	return cmp(a[1], b[1])
-
-	
-	
-todosDados= lerArquivo("treinamento.csv")
-#linha1 = todosDados[0].split(",")
-cont = 0 
-dadosSplit =[]
-#dando o split nos dados 
-for i in todosDados:
-	if (cont == 0):
-		cont = cont + 1
-		pass
-	dadosSplit.append(i.split(","))
-
-
-listaPassageirosSplit =[]
-
-#separando dados como split
-for i in dadosSplit:
-	if ((i[0] == "0" )or( i[0]=="1")):
-		listaPassageirosSplit.append(i)
-
-
-#cria objetos
-listaPassageiros = []
-for i in listaPassageirosSplit:
-	listaPassageiros.append(Passageiro(i[0],i[1], i[2],i[3], i[4], i[5],i[6],i[7],i[8],i[9],i[10],i[11] ))
-
-
-
-#ler dados de teste
-#dadosTeste = lerArquivo("teste.csv")
-#calcular todas as distancias
-Passageiro(i[0],i[1], i[2],i[3], i[4], i[5],i[6],i[7],i[8],i[9],i[10],i[11] )
-
-teste = Passageiro("",3,"O'Sullivan"," Miss. Bridget Mary","female",37,0,0,330909,7.6292,"","Q")
-print (teste.idade)
+#teste = Passageiro("",3,"O'Sullivan"," Miss. Bridget Mary","female",100,0,0,330909,7.6292,"","Q")
+teste = Passageiro("",2,"Quick, ","Mrs. Frederick Charles (Jane Richards)","female",33,0,2,26360,26,"","S")
 
 lista = []
-for i in listaPassageiros:
-	print ("dados\nidade " )
-	print(teste.idade )
-	print (" class " )
-	print (teste.pclass )
-	print (" \nidade" )
-	print (i.idade )
-	if (i.idade == ""): continue   
-	print ("  class")
-	print (i.pclass )
-	print (" nome do segundo elemento ")
-	print (i.nome )
-	print (" \n------\n")
-	lista.append ((calculaDistancia (float (teste.idade), float (teste.pclass), float (i.idade),float ( i.pclass)), i.nome))
+x =GerentePassageiros ("treinamento.csv")
 
-lista.sort()
+for i in x.getLista():
+	if (i.idade == "" or i.pclass == "" or i.tarifa == ""): continue   
+	lista.append ((calculaDistancia (float (teste.idade), float (teste.tarifa), float (i.idade),float ( i.tarifa)), i.nome))
+
+
+	
+
+#lista.sort()
+listaDosM = []
+
+for i in lista:
+	print i[0]
+
+passageirosSobreviveram = 0
+passageirosNaoSobreviveram = 0
+print "-------o lista dos M o-------"
 for i in range (0,M):
-	print lista[i]	
+	listaDosM.append (lista[i])
+	print lista[i]
 
+	
+#verificar quantidade de passageiros que sobreviveram e que não sobreviveram
+for i in listaDosM:
+	for passageiro in x.getLista():
+		if (passageiro.nome == i[1]):
+			if (int (passageiro.sobreviveu) == 1):
+				passageirosSobreviveram = passageirosSobreviveram + 1
+			elif(int (passageiro.sobreviveu) == 0): 
+				passageirosNaoSobreviveram = passageirosNaoSobreviveram + 1
+			break
 
-
-
+print ("passageiros que não sobreviveram = ",passageirosNaoSobreviveram)
+print ("passageiros que sobreviveram = ",passageirosSobreviveram)
